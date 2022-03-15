a5f03a6b28bc04e5bb49ce42a4344d44b9a86ca0
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-03-15 16:12:04 -0300

    Projeto Criado

---
+++ desafio-java/src/main/java/com/desafiojava/DesafioJavaApplication.java
@@ -0,0 +1,13 @@
+package com.desafiojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaApplication.class, args);
+	}
+
+}
---
+++ desafio-java/src/test/java/com/desafiojava/DesafioJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
